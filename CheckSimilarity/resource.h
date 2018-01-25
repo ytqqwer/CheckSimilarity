96e562f25b91379633a75ad3159d2e037ce57daa
ytqqwer <ytqqwer@qq.com>
2018-01-25 16:50:29 +0800

    完成代码迁移

---
+++ CheckSimilarity/resource.h
@@ -0,0 +1,68 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 CheckSimilarity.rc 使用
+//
+#define IDC_MYICON                      2
+#define IDD_SENSEALIGNMENT_DIALOG       101
+#define IDS_APP_TITLE                   102
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_SENSEALIGNMENT              107
+#define IDI_SMALL                       108
+#define IDC_SENSEALIGNMENT              109
+#define IDC_CHECKSIMILARITY             110
+#define IDD_CHECKSIMILARITY_DIALOG      111
+#define IDI_CHECKSIMILARITY             112
+#define IDR_MAINFRAME                   128
+#define ID_PART_OF_SPEECH_N             131
+#define ID_PART_OF_SPEECH_V             132
+#define IDD_DETAIL                      132
+#define ID_PART_OF_SPEECH_A             133
+#define ID_PART_OF_SPEECH_M             134
+#define ID_PART_OF_SPEECH_Q             135
+#define ID_PART_OF_SPEECH_R             136
+#define ID_PART_OF_SPEECH_D             137
+#define ID_PART_OF_SPEECH_P             138
+#define ID_PART_OF_SPEECH_C             139
+#define ID_PART_OF_SPEECH_U             140
+#define ID_PART_OF_SPEECH_E             141
+#define ID_PART_OF_SPEECH_O             142
+#define ID_SEARCH_EDIT                  1001
+#define ID_SEARCH_COMBOBOX              1002
+#define ID_SEARCH_BUTTON                1003
+#define ID_DETAIL_OK                    1021
+#define IDC_MFCPROPERTYGRID1            1026
+#define ID_DICTIONARY_ONE_LISTVIEW      1500
+#define ID_DICTIONARY_TWO_LISTVIEW      1511
+#define ID_DICTIONARY_COLUMN_WORDS      2000
+#define ID_DICTIONARY_COLUMN_ALPHABETIC 2001
+#define ID_DICTIONARY_COLUMN_MEANINGS   2002
+#define ID_DICTIONARY_COLUMN_REMARKS    2003
+#define ID_TEXT_SIMILARITY              3000
+#define ID_TEXT_RELATIONSHIP            3001
+#define ID_TEXT_NEW_RELATIONSHIP        3002
+#define ID_TEXT_STATIC_SIMILARITY       3003
+#define ID_TEXT_STATIC_RELATIONSHIP     3004
+#define ID_TEXT_STATIC_NEW_RELATIONSHIP 3005
+#define ID_EQUAL_BUTTON                 3050
+#define ID_NOT_EQUAL_BUTTON             3051
+#define ID_UNSURE_BUTTON                3052
+#define ID_BELONG_BUTTON                3053
+#define ID_MORE_DETAIL_BUTTON           3100
+#define ID_NEXT_WORD_BUTTON             3101
+#define IDM_OPEN                        10000
+#define IDM_SAVE                        10001
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1027
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
